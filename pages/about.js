0726bb8f1bc0fe4c59c4c68f30f14875688f39c8
Juan Angel Cepeda <jangelcepeda@protonmail.com>
2022-11-20 18:07:17 -0700

    About and Team Pages
    Enrutamiento de paginas de About y Team, solo html

---
+++ pages/about.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function about() {
+  return (
+    <div>About Proof of car
+        <div>
+            Una apliación web unica para consultar la información pertinete a una vehículo
+        </div>
+        <p>
+            La información relevante de los automoviles en una aplicación web
+            disponible para aseguradoras, bancos, gobiernos y ciudadanos.
+            Un sistema de consulta para el ciudadano que brinda seguridad al momento de comprar un vehículo usado
+        </p>
+    </div>
+  )
+}
---
+++ pages/team.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Equipo() {
+  return (
+    <div>Equipo
+        <div>Juan Ángel Cepeda Fernandez</div>
+        <div>Miguel Alan quintana Montaño</div>
+    </div>
+  )
+}
